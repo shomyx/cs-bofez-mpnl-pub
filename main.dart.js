f36c39a6880c7dfe5d71b6730190e8fb4f68d85d
shomyx <mishko.petrovic@gmail.com>
2026-07-12 16:55:48 +0000

    Update from https://github.com/shomyx/mundi/commit/a35ba2a4a0b7f1c5a189ba5cdc46951063fa5237

--- main.dart.js
+++ main.dart.js
@@ -31268,7 +31268,7 @@ $S:2}
 A.aDs.prototype={
 $1(a){var s=A.dn().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/a10d8ac38de835021c8d2f920dbf50a920ccc030/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/83675ed27633283e7fc296c8bca22e841224c096/":s)+a},
 $S:122}
 A.PF.prototype={
 gu(a){var s=this.a
